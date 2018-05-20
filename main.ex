e14eb20e1914aea575034c427037dca386204bcd
Ludvig <ludvigch@kth.se>
2018-05-20 18:03:11 +0200

    added genesis block and testing of mining 2 transactions

--- main.ex
+++ main.ex
@@ -1,5 +1,15 @@
 defmodule Main do
     def start() do
-        
+        genesis = Block.new(1, 1, 100, [])
+        chain = spawn_link(fn -> Blockchain.blockchain([genesis], []) end)
+        send(chain, {:mine, 13})
+        send(chain, {:mine, 13})
+        send(chain, {:chain, self()})
+        receive do
+            x -> x
+        end
     end
+
+    # 1. genesis block for chain
+    # 2. 
 end
